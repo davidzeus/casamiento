959a7aebf9ffd1f935810d881da544e6f5ea2f4c
David Gonzalez <cdgonzalez@HCJSM.local>
2023-08-17 13:17:51 -0300

    agrego excel y login

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,26 @@
+//import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import ExcelJS from 'exceljs'
+import View from '@ioc:Adonis/Core/View'
+
+export default class AuthController {
+  public async login({ request, response }) {
+    const { email } = request.all()
+    // Validar con el archivo Excel
+    const workbook = new ExcelJS.Workbook()
+    await workbook.xlsx.readFile('Copia_de_Confirmacion.xlsx') // Ruta al archivo Excel
+    const worksheet = workbook.getWorksheet(1)
+
+    let storedEmails: any = []
+    worksheet.eachRow((row, rowNumber) => {
+      if (rowNumber !== 1) {
+        const mail = row.getCell(5).value
+        storedEmails.push(mail)
+      }
+    })
+    if (storedEmails.includes(email)) {
+      return View.render('main', { email })
+    } else {
+      return View.render('errors.unauthorized')
+    }
+  }
+}
